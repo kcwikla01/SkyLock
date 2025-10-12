81976ed4320edb8da9559ff32598c1f7d12f72c3
kcwikla01 <kamilcwikla01@gmail.com>
2025-10-12 17:53:01 +0200

    Prepare endpoints crud operation triggers  in controller

---
+++ Skylock.WEB/Controllers/FileController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Skylock.WEB.Controllers
+{
+    [ApiController]
+    [Route("api/File")]
+    public class FileController : Controller
+    {
+
+        [Authorize]
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile()
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("download/{fileId}")]
+        public async Task<IActionResult> DownloadFile(string fileId)
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpDelete("delete/{fileId}")]
+        public async Task<IActionResult> DeleteFile(string fileId)
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("GetFiles")]
+        public async Task<IActionResult> GetFiles()
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("GetFileMetadata/{fileId}")]
+        public async Task<IActionResult> GetFileMetadata(string fileId)
+        {
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("GetAllFiles")]
+        public async Task<IActionResult> GetAllFiles()
+        {
+            return Ok();
+        }
+    }
+}
